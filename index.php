c511641fc934eebd6c1b4521ac9ce5a59999d00d
Your Name <your.email@example.com>
2026-08-23 17:16:51 +0200

    added new file:   index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    echo "<h1>Welcome to Ticketheaven!</h1>";
+?>
+
+<!DOCTYPE HTML>
+	<html lang=“eng”>
+	<head>
+		<meta charset="utf-8">
+		<title>Index</title>
+	</head>
+	<body>
+
+
+	</body>
+</html>
+
+ 
